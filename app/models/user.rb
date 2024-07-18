53f320aaee0bf8a9aeffa8bcd941191cc0768224
Ben Hood <benhood97@btinternet.com>
2024-07-17 18:14:18 -0700

    create user authenticate class method and specs

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,16 @@
 class User < ApplicationRecord
   has_secure_password
   validates :email, presence: true, uniqueness: true
+  validates :password_confirmation, presence: true, if: -> { password.present? }
+  validates :password, presence: true, length: {minimum: 6}
+
+  def self.authenticate_with_credentials (email, password)
+    stripped_email = email.strip.downcase
+    user = User.find_by(email: stripped_email)
+    if user && user.authenticate(password)
+      return user
+    else
+      return nil
+    end
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -15,6 +15,14 @@ RSpec.describe User, type: :model do
       user = User.new(name: 'Bob Green', email: 'bob@bob.com', password_confirmation: 'password1')
       expect(user).not_to be_valid
     end
+    it 'is not valid without a password confirmation given' do
+      user = User.new(name: 'Bob Green', email: 'bob@bob.com', password: 'password')
+      expect(user).not_to be_valid
+    end
+    it 'is not valid when password is under 6 characters' do
+      user = User.new(name: 'Bob Green', email: 'bob@bob.com', password: 'pass', password_confirmation: 'pass')
+      expect(user).not_to be_valid
+    end
     it 'is not valid when email already exists in the database' do
       user1 = User.create(name: 'Bob Green', email: 'bob@bob.com', password: 'password', password_confirmation: 'password')
       user2 = User.new(name: 'Bob Greeny', email: 'bob@bob.com', password: 'password1', password_confirmation: 'password1')
@@ -28,5 +36,40 @@ RSpec.describe User, type: :model do
       user = User.new(name: 'Bob Green', password: 'password', password_confirmation: 'password1')
       expect(user).not_to be_valid
     end
+  end
+    describe '.authenticate_with_credentials' do
+      let! (:user) {User.create(name: 'Bob Green', email: 'bob@bob.com', password: 'password', password_confirmation: 'password')}
+
+      context 'when the credentials are valid' do
+        it 'returns the user' do
+          authenticated_user = User.authenticate_with_credentials('bob@bob.com', 'password')
+          expect(authenticated_user).to eq(user)
+        end
+      end
+
+      context 'when credentials are invalid' do
+        it 'returns nil when email is incorrect' do
+          authenticated_user = User.authenticate_with_credentials('bob@bobby.com', 'password')
+          expect(authenticated_user).to be_nil
+        end
+        it 'returns nil when password is incorrect' do
+          authenticated_user = User.authenticate_with_credentials('bob@bob.com', 'password1')
+          expect(authenticated_user).to be_nil
+        end
+      end
+
+      context 'when email has trailing spaces' do
+        it 'authenticates the user successfully' do
+          authenticated_user = User.authenticate_with_credentials(' bob@bob.com ', 'password')
+          expect(authenticated_user).to eq(user)
+        end
+      end
+
+      context 'when email has a mix of cases' do
+        it 'authenticates the user successfully' do
+          authenticated_user = User.authenticate_with_credentials('Bob@Bob.com', 'password')
+          expect(authenticated_user).to eq(user)
+        end
+      end
   end
 end
